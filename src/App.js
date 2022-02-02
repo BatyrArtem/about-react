52b6fea206c87b30bb81884d50e486a2edf6861c
BatyrArtem <artembatyr88@gmail.com>
2022-02-02 14:59:04 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Main from './components/Main.js'
+import Navbar from './components/Navbar'
+
+export default function App() {
+
+    return(
+        <div className='container'>
+        <Main />
+        <Navbar />
+        </div>
+        
+    )
+}
